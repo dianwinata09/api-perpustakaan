d932294dac718fd703ec4b16dc02a6a5d51ff6ff
dianwinata09 <msocialonly@gmail.com>
2023-02-16 14:56:38 +0700

    Auth

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,20 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuthC;
 use App\Http\Controllers\PostC;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
     return 'Hello World API !';
 });
-Route::get('/posts', [PostC::class, 'index']);
-Route::get('/posts/{id}', [PostC::class,'detail']);
+Route::get('/posts', [PostC::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/posts/{id}', [PostC::class,'detail'])->middleware(['auth:sanctum']);
+
+Route::post('/login', [AuthC::class, 'login']);
+
+Route::get('/password', function (){
+    return Hash::make('rahasia');
+});
+    
